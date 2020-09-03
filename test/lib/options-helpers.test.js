221b2b7023e16fe2300764b556bfc558726a5f13
costa.tsiakouris <w@kisdVYa$&FYokP03&H7ndZFxnue242rHygzqk1E&lIh06#JGAp0XbF9WW6Oyb!gU1Qfy5%hLbNjrAfukwu4auJM4Cxb9GMMh1>
2020-09-03 17:26:21 +0100

    add test for options-helpers

---
+++ test/lib/options-helpers.test.js
@@ -0,0 +1,26 @@
+const {validateOptions} = require('../../lib/options-helpers');
+
+describe('Options', () => {
+    test('should get options and files back', () => {
+        const validatedResults = validateOptions(['fakeSearch'], {concurrency: 3, silent: true});
+        const expectedResults = {processedGlobArray: [], validatedOptions: {concurrency: 3, silent: true}};
+
+        expect(validatedResults).toEqual(expectedResults);
+    })
+
+    test('passing an unexpected option should throw an error', () => {
+        expect(() => validateOptions([], {fakeOption: false})).toThrowError();
+    })
+
+    test('passing an invalid value for concurrency should throw an error', () => {
+        expect(() => validateOptions([], {concurrency: '2'})).toThrowError();
+    })
+
+    test('passing an invalid value for silent should throw an error', () => {
+        expect(() => validateOptions([], {concurrency: 'false'})).toThrowError();
+    })
+
+    test('invalid type for globArray should throw an error', () => {
+        expect(() => validateOptions(null, {})).toThrowError(new Error('globArray should be an Array'));
+    });
+})
